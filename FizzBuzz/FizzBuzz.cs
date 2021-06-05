fc30dfa6dad64bebda74a289fb8c7da987a1d453
adekeagle <2726949-adekeagle@users.noreply.gitlab.com>
2021-06-05 23:04:07 +0200

    #1 initial commit

---
+++ FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,17 @@
+﻿namespace FizzBuzz
+{
+    class FizzBuzz
+    {
+        public string GetInfo(int number)
+        {
+            if(number % 3 == 0 && number % 5 == 0)
+                return "FizzBuzz";
+            else if(number % 3 == 0)
+                return "Fizz";
+            else if(number % 5 == 0)
+                return "Buzz";
+            else
+                return number.ToString();
+        } 
+    }
+}
---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                FizzBuzz fizzBuzz = new FizzBuzz();
+                Console.WriteLine("Podaj liczbę : ");
+
+
+                while (true)
+                {
+                    int userNumber = GetNumber();
+
+                    string result = fizzBuzz.GetInfo(userNumber);
+
+                    Console.WriteLine(result);
+
+                    Console.WriteLine("Aby wyjść z gry naciśnij przycisk 'N'");
+                }
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadLine();
+
+            }
+        }
+
+        private static int GetNumber()
+        {
+            while (true)
+            {
+                var userDecision = Console.ReadLine();
+
+                if (userDecision.ToUpper() == "N")
+                    Environment.Exit(0);
+
+                if (!int.TryParse(userDecision, out int input))
+                {
+                    Console.WriteLine("Podana wartość nie jest prawidłowa");
+                    continue;
+                }
+
+                return input;
+
+            }
+        }
+    }
+}
